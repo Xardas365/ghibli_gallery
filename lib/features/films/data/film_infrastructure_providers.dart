e3a50b828bdae1fb216d1f1c36b85a13a0c3e40d
Juraj Kršák <krsakjuro@gmail.com>
2026-06-06 10:48:57 +0200

    feat: wire core film providers

---
+++ lib/features/films/data/film_infrastructure_providers.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ghibli_entry/core/network/network_providers.dart';
+import 'package:ghibli_entry/features/films/data/api/film_api.dart';
+import 'package:ghibli_entry/features/films/data/film_repository_impl.dart';
+import 'package:ghibli_entry/features/films/data/shared_preferences_favorite_movie_storage.dart';
+import 'package:ghibli_entry/features/films/domain/favorite_movie_storage.dart';
+import 'package:ghibli_entry/features/films/domain/film_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final filmApiProvider = Provider<FilmApi>((ref) {
+  return FilmApi(ref.watch(apiClientProvider));
+});
+
+final filmRepositoryProvider = Provider<FilmRepository>((ref) {
+  return FilmRepositoryImpl(api: ref.watch(filmApiProvider));
+});
+
+final sharedPreferencesProvider = FutureProvider<SharedPreferences>((ref) {
+  return SharedPreferences.getInstance();
+});
+
+final favoriteMovieStorageProvider = FutureProvider<FavoriteMovieStorage>((
+  ref,
+) async {
+  final preferences = await ref.watch(sharedPreferencesProvider.future);
+  return SharedPreferencesFavoriteMovieStorage(preferences: preferences);
+});
